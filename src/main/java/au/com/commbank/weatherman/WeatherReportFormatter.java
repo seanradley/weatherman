4985cd2e25ffb0990829908159cae943fad2e4e2
Sean Radley <seanr@qualica.com>
2016-09-23 02:35:13 +0200

    Fixed decimal formatting issue that omits leading 0 Changed default humidity to allow for more variaty in weather conditions

--- src/main/java/au/com/commbank/weatherman/WeatherReportFormatter.java
+++ src/main/java/au/com/commbank/weatherman/WeatherReportFormatter.java
@@ -11,16 +11,16 @@ public class WeatherReportFormatter {
 
         StringBuffer stringBuffer = new StringBuffer();
         stringBuffer.append(weatherReport.getWeatherStation().getWeatherStationIATACode()).append(SEPARATOR);
-        stringBuffer.append(new DecimalFormat("#.00").format(weatherReport.getWeatherStation().getLatitude())).append(",")
-                .append(new DecimalFormat("#.00").format(weatherReport.getWeatherStation().getLongitude())).append(",")
+        stringBuffer.append(new DecimalFormat("#0.00").format(weatherReport.getWeatherStation().getLatitude())).append(",")
+                .append(new DecimalFormat("#0.00").format(weatherReport.getWeatherStation().getLongitude())).append(",")
                 .append(weatherReport.getWeatherStation().getElevation()).append(SEPARATOR);
         stringBuffer.append(weatherReport.getDateTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"))).append(SEPARATOR);
         stringBuffer.append(weatherReport.getWeatherConditions()).append(SEPARATOR);
 
         stringBuffer.append(weatherReport.getTemperature() >= 0 ? "+" : "");
-        stringBuffer.append(new DecimalFormat("#.0").format(weatherReport.getTemperature())).append(SEPARATOR);
+        stringBuffer.append(new DecimalFormat("#0.0").format(weatherReport.getTemperature())).append(SEPARATOR);
 
-        stringBuffer.append(new DecimalFormat("#.0").format(weatherReport.getPressure())).append(SEPARATOR);
+        stringBuffer.append(new DecimalFormat("#0.0").format(weatherReport.getPressure())).append(SEPARATOR);
         stringBuffer.append(new DecimalFormat("#").format(weatherReport.getHumidity()));
 
         return stringBuffer.toString();
