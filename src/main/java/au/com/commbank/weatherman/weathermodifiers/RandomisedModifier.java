ed2302b36f5f1fcb7d0f50c5da4b5bba8bdf16da
Sean Radley <seanr@qualica.com>
2016-09-23 02:08:23 +0200

    Added modifier for weather conditions

--- src/main/java/au/com/commbank/weatherman/weathermodifiers/RandomisedModifier.java
+++ src/main/java/au/com/commbank/weatherman/weathermodifiers/RandomisedModifier.java
@@ -1,5 +1,6 @@
 package au.com.commbank.weatherman.weathermodifiers;
 
+import au.com.commbank.weatherman.WeatherConditions;
 import au.com.commbank.weatherman.WeatherReport;
 
 public class RandomisedModifier implements WeatherModifier {
@@ -11,13 +12,14 @@ public class RandomisedModifier implements WeatherModifier {
     public void applyModifier(WeatherReport report) {
         modifyTemperature(report);
         modifyHumidity(report);
+        modifyConditions(report);
     }
 
     private void modifyTemperature(WeatherReport report) {
         double random = Math.random() * 2 - 1;
         double tempDelta = random * TEMPERATURE_SCALAR * report.getTemperature();
 
-        report.setTemperature(report.getTemperature() + tempDelta );
+        report.setTemperature(report.getTemperature() + tempDelta);
     }
 
     private void modifyHumidity(WeatherReport report) {
@@ -25,5 +27,20 @@ public class RandomisedModifier implements WeatherModifier {
         double humidityDelta = random * HUMIDITY_SCALAR * report.getHumidity();
 
         report.setHumidity((int) (report.getHumidity() + humidityDelta));
+    }
+
+    private void modifyConditions(WeatherReport report) {
+        double random = Math.random();
+
+        boolean isPrecipitation = report.getHumidity() > 85 && random > 0.5;
+        if (isPrecipitation) {
+            if (report.getTemperature() > 2.0) {
+                report.setWeatherConditions(WeatherConditions.Rain);
+            } else {
+                report.setWeatherConditions(WeatherConditions.Snow);
+            }
+        } else if (report.getHumidity() > 80) {
+            report.setWeatherConditions(WeatherConditions.Cloud);
+        }
     }
 }
