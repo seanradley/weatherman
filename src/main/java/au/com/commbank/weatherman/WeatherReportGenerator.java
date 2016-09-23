09adef9c0be98a3b92d8072878782ff2f5c196e5
Sean Radley <seanr@qualica.com>
2016-09-23 02:27:24 +0200

    Added additional weather stations

--- src/main/java/au/com/commbank/weatherman/WeatherReportGenerator.java
+++ src/main/java/au/com/commbank/weatherman/WeatherReportGenerator.java
@@ -21,6 +21,13 @@ public class WeatherReportGenerator {
         stations.add(new WeatherStation("SYD", -33.86, 151.21));
         stations.add(new WeatherStation("SIN", 1.359211,103.989306));
         stations.add(new WeatherStation("DME", 55.408611, 37.906111));
+        stations.add(new WeatherStation("JNB", -26.139167, 28.246111));
+        stations.add(new WeatherStation("JFK", 40.639722, -73.778889));
+        stations.add(new WeatherStation("LHR", 51.4775, -0.461389));
+        stations.add(new WeatherStation("FCO", 41.800278, 12.238889));
+        stations.add(new WeatherStation("ANC", 61.174167, -149.998333));
+        stations.add(new WeatherStation("TXL", 52.559722, 13.287778));
+        stations.add(new WeatherStation("LOS", 6.577222, 3.321111));
 
         WeatherReportGenerator generator = WeatherReportGenerator.createGeneratorWithAllModifiers();
         List<WeatherReport> reports = null;
